5a6a55ec6a008b9311045820dc5715bd56caca5e
fargustian <aleksfers@icloud.com>
2018-09-06 22:20:47 +0500

    Integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@
 class PagesController < ApplicationController  
   def home
     @posts = Blog.all
+    @skills = Skill.all
   end
 
-  def about; end
+  def about
+  end
 
   def contact; end
 end
