61a520a4160e6d22f6fe2b3fac7643f2344132a4
fargustian <aleksfers@icloud.com>
2018-09-11 20:51:34 +0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
+
+  root to: 'pages#home'
 end
