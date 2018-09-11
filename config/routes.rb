221e9627e14ba21dcd9c029803f0f96101e12b2a
fargustian <aleksfers@icloud.com>
2018-09-11 21:21:36 +0500

    Integrate friendly routes for blog post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs, except: [:show]
+  get 'blog/:id', to: 'blogs#show', as: 'blog_show'
 
   root to: 'pages#home'
 end
