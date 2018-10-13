8d4661bd81c50e7709f2a26b96edd8c82eb587dd
Aleksey Peresmekhin <aleksfers@icloud.com>
2018-10-13 16:31:33 +0500

    Implement concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates :title, :body, :main_image, :thumb_image, presence: true
 
   def self.angular
@@ -11,7 +13,7 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
     # it says for example if main image equal nil, set default url
-    self.main_image ||= 'https://via.placeholder.com/600x400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= Placeholder.image_generator(width: '600', height: '400')
+    self.thumb_image ||= Placeholder.image_generator(width: '350', height: '200')
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class Skill < ApplicationRecord
+  include Placeholder
   validates :title, :percent_utilized, presence: true
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    # it says for example if main image equal nil, set default url
+    self.badge ||= Placeholder.image_generator(width: '250', height: '250')
+  end
 end
