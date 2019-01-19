b12fbffcb2f6a220a5a910788134a970ed4f2910
Aleksey Peresmekhin <aleksfers@icloud.com>
2019-01-19 22:16:23 +0500

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,38 @@ module ApplicationHelper
   def copyright_generator
     DevcampViewTool::Renderer.copyright 'Aleksey Peresmekhin', 'All right reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home',
+      },
+      {
+        url: about_me_path,
+        title: 'About me',
+      },
+      {
+        url: contact_path,
+        title: 'Contact',
+      },
+      {
+        url: blogs_path,
+        title: 'Blog',
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio',
+      },
+    ]
+  end
+
+  def nav_helper  style, tag_type
+    safe_join nav_items
+      .map { |item| "<#{tag_type}><a href=\"#{item[:url]}\" class=\"#{style} #{active? item[:url]}\">#{item[:title]}</a></#{tag_type}>".html_safe}
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
