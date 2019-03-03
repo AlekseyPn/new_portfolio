370028c313d45f423de58568fdf310b593f0297a
fargustian <aleksfers@icloud.com>
2019-03-03 20:01:15 +0500

    Integrated cocoon for portfolio items nested form component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,8 +21,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    # this is harcode for create technologies
-    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -66,7 +64,7 @@ class PortfoliosController < ApplicationController
                                       :main_image,
                                       :thumb_image,
                                       :body,
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name, :_destroy])
   end
 
   def set_portfolio_item
