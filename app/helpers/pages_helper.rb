a4901043b3410c8d09f255916813aaa491a3e816
fargustian <aleksfers@icloud.com>
2019-03-04 22:42:07 +0500

    Built twitter parser for tweet links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+  URL_REGEX = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+  def twitter_parser tweet
+    tweet.gsub(URL_REGEX) do |url|
+      "<a href='#{url}' target='_blank' class='tweet-link'>#{url}</a>"
+    end.html_safe
+  end
+  
 end
